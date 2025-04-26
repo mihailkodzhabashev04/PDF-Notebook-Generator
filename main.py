2f4619213f0123558c3ebe6567f4a51c50d20845
mihailkodzhabashev04 <misho.kodjabashev@gmail.com>
2025-04-26 12:34:39 +0300

    Add pages

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
+    #set the header
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10,20, 200, 20)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     #set the footer for the master page
     pdf.ln(265)
@@ -23,6 +26,9 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
